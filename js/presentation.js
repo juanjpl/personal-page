681b64b5d248a3573d044b5959b8424f8c2bc2b9
Juan Lima <juan_jpl@hotmail.com>
2025-01-04 01:34:57 -0300

    fix colours

--- js/presentation.js
+++ js/presentation.js
@@ -1,17 +1,16 @@
 
 
 
-
-function clickMe (){
-    setTimeout(() => {
-    const caja = document.getElementById("box")
-    caja.classList.add("box-motion")
-      }, "100");
- 
-}
-
 window.onload = function(e){ 
     console.log("Bienvenidos")
-    const caja = document.getElementById("box")
-    caja.classList.add("box-motion")
+  
+    setTimeout(() => {
+        const caja = document.getElementById("box")
+        caja.classList.add("box-motion")
+          }, "4000");
+
+          /*
+          setTimeout(()=>{window.location = '../index.html' },5000)
+          */
 }
+    
