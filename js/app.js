affd3862b7f4bb3f415d4977495ce27bc9604b2e
juanjpl <juan_jpl@hotmail.com>
2025-01-06 12:09:20 -0300

    fix

--- js/app.js
+++ js/app.js
@@ -56,3 +56,11 @@ function clickMeTwo (){
     caja.classList.add("box-motion-two")
 }
 
+
+document.querySelectorAll('.js-marquee').forEach(function(e) {
+    var letter = e.querySelector('span');
+    for (counter = 1; counter <= 3; ++counter) {
+        var clone = letter.cloneNode(true);
+        letter.after(clone);
+    }
+})
